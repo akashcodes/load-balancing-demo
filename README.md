05e0cfbb06613cf6d6af8f57f978b434559deaea
akashcodes <akashdotcodes@gmail.com>
2020-02-08 08:10:13 +0530

    using pug for rendering dad jokes

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Server Auto-Scaling
+# Server Load Balancing Performance Comparison
 
 ## To demonstrate load balacing on AWS and GCP. Created for CS6847 Programming Assignment #1 at Indian Institute of Technology Madras.
--- nodejs-server/index.js
+++ nodejs-server/index.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const fs = require('fs');
-var path = require('path');
+const path = require('path');
+const pug = require("pug");
 
+const jokes = require("./jokes.json")
 
 const app = express();
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, '/views'));
 
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'html/index.html'));
+    res.render('index', {joke: jokes["jokes"][Math.floor(Math.random() * jokes["jokes"].length)]});
 });
 
 app.listen(port, () => {
