8bc330f56fc197b6918cbdf7e8e11690d3260440
akashcodes <akashdotcodes@gmail.com>
2020-02-08 11:36:38 +0530

    Added new server functionalities

--- nodejs-server/index.js
+++ nodejs-server/index.js
@@ -1,20 +1,82 @@
 const express = require("express");
-const fs = require('fs');
 const path = require('path');
-const pug = require("pug");
 
 const jokes = require("./jokes.json")
 
 const app = express();
+
+
+// Make express app to use view engine as pug
 app.set('view engine', 'pug');
+
+
+// Set views directory
 app.set('views', path.join(__dirname, '/views'));
 
+// Server PORT
 const port = 3000;
 
+//Detault message to send at GET /
+const WELCOME_RESPONSE = `
+    <html>
+        <head>
+            <title>Welcome!</title>
+        </head>
+        <body>
+            <u><h3>Hello, stranger! Welcome to my Cloud Computing assignment.</h3></u>
+            <li>GET /dad-joke ; to get a random Dad Joke.</li>
+            <li>GET /dad-joke/:number ; with param(number=[1-99]) to get a particular Dad Joke Indexed[1 to 99].</li>
+            <li>GET /palindrome/:string ; to check whether the "string" is a palindrome string</li>
+            <br/><br/>
+            Adios!
+    </html>
+`;
+
+
+// Default router
 app.get("/", (req, res) => {
+    res.send(WELCOME_RESPONSE);
+});
+
+
+// GET dad joke
+app.get("/dad-joke", (req, res) => {
     res.render('index', {joke: jokes["jokes"][Math.floor(Math.random() * jokes["jokes"].length)]});
 });
 
+
+// GET dad-joke at index 'number'
+app.get("/dad-joke/:number", (req, res) => {
+    if(req.params["number"] <= 0 || req.params["number"] >= 100)
+        res.send("Invalid index. Please enter an index i from 1 to 99")
+    var i = req.params["number"];
+    res.render('index', {joke: jokes["jokes"][i]});
+});
+
+
+// GET check is string is palidrome
+app.get("/palindrome/:string", (req, res) => {
+    var str = req.params["string"];
+    var flag = true;
+    var l = 0;
+    var r = str.length - 1;
+    while(l < r) {
+        if(str[l] != str[r]) {
+            flag = false;
+            break;
+        }
+        l++;
+        r--;
+    }
+
+    if(flag)
+        res.send("True");
+    else
+        res.send("False");
+});
+
+
+// Start listening
 app.listen(port, () => {
     console.log(`Listening to port ${port}`);
 });
