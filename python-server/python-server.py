0c1572f1f1a2ca40e6436f813c8924defd1e6d01
Akash Mishra <akash@Akashs-MacBook-Pro.local>
2020-02-10 00:37:59 +0530

    Create server, client files

--- python-server/python-server.py
+++
@@ -1,9 +0,0 @@
-from aiohttp import web
-
-async def hello(request):
-    return web.FileResponse('./index.html')
-
-
-app = web.Application()
-app.add_routes([web.get("/", hello)])
-web.run_app(app, port=8000)
