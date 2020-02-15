a03b52ee614953e90b5299ca7d0cc3bdd31345a3
Akash Mishra <akash@Akashs-MacBook-Pro.local>
2020-02-15 17:38:58 +0530

    plot.py

--- client/locust-client.py
+++ client/locust-client.py
@@ -4,15 +4,16 @@ class WebsiteTasks(TaskSet):
     def on_start(self):
         self.client.get("/")
     
-    @task
-    def index(self):
+    @task(60)
+    def dad_joke(self):
         self.client.get("/dad-joke")
-        
-    @task
-    def about(self):
+    
+
+    @task(5)
+    def fetch(self):
         self.client.get("/fetch")
 
 
 class WebsiteUser(HttpLocust):
     task_set = WebsiteTasks
-    wait_time = between(5, 15)
+    wait_time = between(1, 1)
---
+++ plot.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+OUTPUT_ROOT = "../output/"
+
+if __name__ == "__main__":
+    
+    utype = input("""
+    Select:
+    1 - Load balanced sampling
+    2 - Load Balancing with auto-scaling
+    3 - Without load balanced sampling
+    """)
+    utype = int(utype)
+
+    dire = OUTPUT_ROOT+"without load balancing/"
+
+    url = None
+    if utype == 1:
+        print("Load Balanced Sample")
+        dire = OUTPUT_ROOT+"with load balancing/"
+    elif utype == 2:
+        print("Load Balanced Sample")
+        dire = OUTPUT_ROOT+"with load balancing autoscaling/"
+    else:
+        print("Sampling without load balancing")
+    
+    fname = input("Enter file name where to save data: ")
+    fname = dire+fname
+
+    data = np.loadtxt(fname)
+    print(np.mean(data))
+    plt.plot(data)
+    #plt.show()
